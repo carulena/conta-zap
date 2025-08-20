367a76d6ed92ac6e885e1afba19eadff2f58db40
anecarolina.simoes <anecarolina.simoes@gmail.com>
2025-08-20 15:48:17 -0300

    vercel

--- api/recebeMensagem.py
+++ api/recebeMensagem.py
@@ -3,12 +3,12 @@ from http.server import BaseHTTPRequestHandler
  
 # Configura o matplotlib para rodar em /tmp (Vercel serverless)
 os.environ['MPLCONFIGDIR'] = '/tmp'
-import zipfile
 from io import BytesIO
 from telegram import Update, Bot
 import api.analisaDados as d
 import pandas as pd
 import json
+import asyncio
 
 
 # Inicializa o bot globalmente (sem criar servidor local)
@@ -17,11 +17,14 @@ bot = Bot(token=TOKEN)
 
 class handler(BaseHTTPRequestHandler):
     
-   async def do_POST(self):
+   def do_POST(self):
         content_length = int(self.headers.get('Content-Length', 0))
         body = self.rfile.read(content_length)  # bytesupdate_json = json.loads(body)         # agora é dict
         update_json = json.loads(body)         # agora é dict
         update = Update.de_json(update_json, bot)
         chat_id = update.message.chat.id
-        await bot.send_message(chat_id=chat_id, text="Recebi o ZIP!")
-        return {"status": 200, "body": "ok"}
+        asyncio.run(bot.send_message(chat_id=chat_id, text="Recebi o ZIP!"))
+        
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"ok")
