19ae1ad9e904751e8ff599654636a8b38bd68892
anecarolina.simoes <anecarolina.simoes@gmail.com>
2025-08-20 15:41:24 -0300

    vercel 345

--- api/recebeMensagem.py
+++ api/recebeMensagem.py
@@ -8,6 +8,7 @@ from io import BytesIO
 from telegram import Update, Bot
 import api.analisaDados as d
 import pandas as pd
+import json
 
 
 # Inicializa o bot globalmente (sem criar servidor local)
@@ -17,9 +18,9 @@ bot = Bot(token=TOKEN)
 class handler(BaseHTTPRequestHandler):
     
     def do_POST(self):
-        print(self.request)
-        update_json = self.request.get_json()
+        content_length = int(self.headers.get('Content-Length', 0))
+        body = self.rfile.read(content_length)  # bytesupdate_json = json.loads(body)         # agora é dict
+        update_json = json.loads(body)         # agora é dict
         update = Update.de_json(update_json, bot)
         chat_id = update.message.chat.id
         bot.send_message(chat_id=chat_id, text="Recebi o ZIP!")
-       
